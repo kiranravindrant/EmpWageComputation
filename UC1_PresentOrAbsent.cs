45605b436a45fd08f7617d1d39878dc89a699f19
kiranravindrant <kiranravindrant@gmail.com>
2021-07-06 01:14:49 +0530

    PresentorAbsent Added

---
+++ UC1_PresentOrAbsent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class UC1_PresentOrAbsent
+    {
+        public static void empCheck()
+        {
+            int IS_FULL_Time = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_Time)
+                Console.WriteLine("Emp is present");
+            else
+                Console.WriteLine("Emp is absent");
+        }
+    }
+}
+   
