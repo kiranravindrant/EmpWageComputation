c6f7858611c24692e053e727c773668d42b17301
kiranravindrant <kiranravindrant@gmail.com>
2021-07-06 02:20:32 +0530

    Final Solution Added

---
+++ FinalSolution/IComputeEmpWage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation.FinalSolution
+{
+   // interface IComputeEmpWage
+    //{
+        public interface IComputeEmpWage
+        {
+            void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+            void computeEmpWage();
+
+            int getTotalWage(string company);
+        }
+
+    }
+
